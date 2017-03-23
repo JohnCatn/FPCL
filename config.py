e6f34d25f96529ecf2d79f703e84dbedd4f37750
John Catnach <JohnCatn@users.noreply.github.com>
2017-03-23 10:07:03 +0000

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+# Twilio Account Settings
+account_sid = "TWILIO ACCOUNT SID"
+auth_token  = "TWILIO AUTH TOKEN"
+from = 	"TWILIO FROM NUMBER"
+to = "TO NUMBER"
+
+#Free Postcode Lottert personal URL - click on teh link in the daily email which authenticates you automatically and copy the reminder ID
+fpc_url = "https://freepostcodelottery.com/?reminder=YOUR_REMINDER_ID"
